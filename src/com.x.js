017490d5a243b6485d01fb5d452a853b01f55a21
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2024-09-05 18:51:41 +0000

    commit

--- src/com.x.js
+++ src/com.x.js
@@ -2,7 +2,7 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 //
 import check from 'check-types';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 //
 import * as eagle from './eagle.js';
 import * as setting from './setting.js';
